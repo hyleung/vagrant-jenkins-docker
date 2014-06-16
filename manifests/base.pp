1930344cecff281079a608664e7cf006d8e0d61d
Ho Yan Leung <hy.leung@gmail.com>
2014-06-16 13:05:58 -0700

    Updated manifest to install docker plugin 0.6.1

--- manifests/base.pp
+++ manifests/base.pp
@@ -22,12 +22,8 @@ node default {
   jenkins::plugin {'git-client':} ~>
   jenkins::plugin {'scm-api':} ~>
   jenkins::plugin {'git':} ~>
-  
-  file { '/var/lib/jenkins/plugins/docker-plugin.hpi':
-    source => '/vagrant/files/docker-plugin.hpi'
+    jenkins::plugin {
+    'docker-plugin':
+      version =>'0.6.1';
   }
-  #jenkins::plugin {
-  #  'docker-plugin':
-  #    version =>'0.6';
-  #}
 }
